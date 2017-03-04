5357a94f773f3719b41a28f0f1352b48ebba40f4
William <williamleber67@gmail.com>
2017-03-04 15:33:12 -0800

    moving circle

--- circlespy.py
+++ circlespy.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, time
 from pygame.locals import *
 WIDTH = 500
 HEIGHT = 400
@@ -15,16 +15,22 @@ WHITE = (255,255,255)
 location = (250,200)
 radius = 45
 color = WHITE
+border = 1
 
 def Circle(surface, color, radius, location = MIDDLE, border = 1):
     pygame.draw.circle(surface, color, location,radius,border)
-c1 = {'rect':pygame.Rect(200,200,20,20), 'color':WHITE, 'dir':UPLEFT}
+c1 = {'topx':250,'topy':200, 'color':WHITE, 'dir':UPLEFT}
 Circle(windowSurface, color, radius, location, 1)
 #last
-pygame.display.update()
 
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    windowSurface.fill(BLACK)
+    if c1['dir'] == UPLEFT:
+        c1['topx'] -= 5
+    pygame.draw.circle(windowSurface, color, (c1['topx'],c1['topy']),radius,border)
+    pygame.display.update()
+    time.sleep(0.02)
