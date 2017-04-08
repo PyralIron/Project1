287fd8f24450db8a2d1a003d4cb12461d4c922ed
William <williamleber67@gmail.com>
2017-04-08 15:56:20 -0700

    2017-04-08

--- circlespy.py
+++ circlespy.py
@@ -38,7 +38,7 @@ c2 = {'name':"c2",'loc':(250,400),'centerx':500,'centery':400, 'color':GREEN, 'd
 c3 = {'name':"c3",'loc':(350,900),'centerx':500,'centery':400, 'color':RED, 'dir':CWISE, 'angle':180,'radius':350, 'speed':0.01, 'mass': 418, 'velocity':[-50,30]}
 c4 = {'name':"c4",'loc':(450,200),'centerx':500,'centery':400, 'color':BLUE, 'dir':CWISE, 'angle':45,'radius':400, 'speed':0.01, 'mass':19.14, 'velocity':[10,50]}
 c5 = {'name':"c5",'loc':(900,100),'centerx':500,'centery':400, 'color':PURPLE, 'dir':CWISE, 'angle':56,'radius':50, 'speed':0.5, 'mass':.00289, 'velocity':[10,50]}
-c6 = {'name':"c6",'loc':(600,400),'centerx':500,'centery':400, 'color':YELLOW, 'dir':CWISE, 'angle':56,'radius':5, 'speed':0.001, 'mass':438500, 'velocity':[0,0]}
+c6 = {'name':"c6",'loc':(500,400),'centerx':500,'centery':400, 'color':YELLOW, 'dir':CWISE, 'angle':56,'radius':5, 'speed':0.001, 'mass':438500, 'velocity':[0,0]}
 planetlist = (c1,c2,c3,c4,c5,c6)
 def CenterofMass(planetlist):
     centerOfMassX = 0
@@ -47,8 +47,8 @@ def CenterofMass(planetlist):
     for i in planetlist:
         totalMass += i['mass']
     for i in planetlist:
-        centerOfMassX += i['mass']*(i['radius']*cos(i['angle'])+i['centerx'])
-        centerOfMassY += i['mass']*(i['radius']*sin(i['angle'])+i['centery'])
+        centerOfMassX += i['mass']*i['loc'][0]
+        centerOfMassY += i['mass']*i['loc'][1]
     centerOfMassX *= 1/totalMass
     centerOfMassY *= 1/totalMass
     pygame.draw.circle(windowSurface, RAND, (floor(centerOfMassX),floor(centerOfMassY)),10,1)
@@ -69,27 +69,44 @@ def revolve(myangle,direction,centerx,centery,myspeed,mycolor,myradius):
         
     pygame.draw.circle(windowSurface, mycolor, (x,y),radius,border)
     return (myangle)
-def velocity(theplanetlist):
+def velocity(i,theplanetlist):
     centerOfMass = CenterofMass(theplanetlist)
-    for i in theplanetlist:
-        force = G*centerOfMass[2]*i['mass']/((centerOfMass[0]-i['loc'][0])**2+(centerOfMass[1]-i['loc'][1])**2)
-        accel = force/i['mass']
-        print ('accel')
-        print (i['name'])
-        print (accel)
-        vel = (int(i['velocity'][0])+accel,int(i['velocity'][1]+accel)
-        i['velocity'] = str(vel)
-        print ("velocity" + i['velocity'])
-def newRevolve(theplanetlist):
-    for i in planetlist:
-        h = h
+    force = G*centerOfMass[2]*i['mass']/((centerOfMass[0]-i['loc'][0])**2+(centerOfMass[1]-i['loc'][1])**2)
+    accel = force/i['mass']
+    displacementX = i['loc'][0]-centerOfMass[0]
+    displacementY = i['loc'][1]-centerOfMass[1]
+    displacement = sqrt(displacementX**2+displacementY**2)
+    accelX = displacement*accel/displacementX 
+    accelY = displacement*accel/displacementY
+    accelVec = (accelX,accelY)
+    newVelX = (i['velocity'][0]+accelVec[0])/50
+    newVelY = (i['velocity'][1]+accelVec[1])/50
+    newVel = (newVelX,newVelY)
+    print ('accel')
+    print (i['name'])
+    print (accelVec)
+    i['velocity'] = newVel
+    print ("velocity" + str(i['velocity'][0]) + "," + str(i['velocity'][1]))
+    positionX = i['loc'][0]+newVel[0]
+    positionY = i['loc'][1]+newVel[1]
+    position = (positionX,positionY)
+    i['loc'] = position
+    print ("position"+str(i['loc'][0]) + "," + str(i['loc'][1]))
+    print ("-------------------------------------")
+    Circle(windowSurface, color, radius, (floor(i['loc'][0]),floor(i['loc'][1])), 1)
+    return(i['loc'])
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     windowSurface.fill(BLACK)
-    velocity(planetlist)
+    velocity(c1,planetlist)
+    velocity(c2,planetlist)
+    velocity(c3,planetlist)
+    velocity(c4,planetlist)
+    velocity(c5,planetlist)
+    velocity(c6,planetlist)
     (myangle) = revolve(c1['angle'],c1['dir'],c1['centerx'],c1['centery'],c1['speed'],c1['color'],c1['radius'])
     c1['angle'] = myangle
     (myangle) = revolve(c2['angle'],c2['dir'],c2['centerx'],c2['centery'],c2['speed'],c2['color'],c2['radius'])
@@ -102,6 +119,7 @@ while True:
     c5['angle'] = myangle
     (myangle) = revolve(c6['angle'],c6['dir'],c6['centerx'],c6['centery'],c6['speed'],c6['color'],c6['radius'])
     c6['angle'] = myangle
+    
     CenterofMass(planetlist)
     pygame.display.update()
     time.sleep(0.02)
