ee25c14b0ac356ca40decf9807b5158ab7b93cf9
William <williamleber67@gmail.com>
2017-07-08 16:13:20 -0700

    changed size of planets to scale,added follow planets, and added framerate and scale display

--- circlespy.py
+++ circlespy.py
@@ -13,6 +13,7 @@ pygame.init()
 time.sleep(0.5)
 BLACK = (0,0,0)
 WHITE = (255,255,255)
+GRAY = (125,125,125)
 GREEN = (0,200,50)
 RUST = (255,69,0)
 ORANGE = (255,140,0)
@@ -35,11 +36,11 @@ speed = 0.1
 sun = (500,400)
 #default: 1 pix = 10^10 m
 scalefactor = 10**10
-G = 6.6741*(10**(-11)/(scalefactor**3))/(10**(-12))*6.91
+G = 6.6741*(10**(-11)/(scalefactor**3))/(10000*(10**(-12)))*6.91
 x = 0
 y = 0
 F = 9.0
-VelConv = 7.3*10**(-5)
+VelConv = 7.3*10**(-5)/100
 planetstuff = []
 windowSurface = pygame.display.set_mode(BOTTOM_R,0,32)
 class Planet:
@@ -53,7 +54,8 @@ class Planet:
         return
     def draw(self, surface, cam, border = 1):
         global scalefactor
-        pygame.draw.circle(surface, self.color, (floor(((self.loc[0])*cam.zoom)-cam.pos[0]+500),floor(((self.loc[1])*cam.zoom)-cam.pos[1]+400)),floor(self.radius*cam.zoom),0)
+        
+        pygame.draw.circle(surface, self.color, (floor(((self.loc[0])*cam.zoom)-cam.pos[0]+500),floor(((self.loc[1])*cam.zoom)-cam.pos[1]+400)),min(floor(self.radius*cam.zoom),2**31-1),0)
 class Camera:
     def __init__(self,initpos,initzoom):
         self.pos = initpos
@@ -99,14 +101,17 @@ def calculate_position(planets):
         target_planet.loc = position
 
 planets = [
-    Planet("Sun",(0,0),1.989*(10**30),[0.0,0.0],5,YELLOW),
-    Planet("Earth",(0,415.21-400),5.97*(10**24),[VelConv*105444.0,0.0],2,CADETBLUE),
-    Planet("Mars",(0,424.923-400),0.642*(10**24),[VelConv*79092.0,0.0],2,RUST),
-    Planet("Jupiter",(0,322.14-400),1898.0*(10**24),[VelConv*44784.0*(-1),0.0],2,PALEGOLD),
-    Planet("Saturn",(0,551.45-400),568.0*(10**24),[VelConv*32724.0,0.0],2,CORAL),
-    Planet("Uranus",(0,700.362-400),86.8*(10**24),[VelConv*23364.0,0.0],2,AQUA),
-    Planet("Neptune",(0,-54.567-400),102.0*(10**24),[VelConv*19332.0*(-1),0.0],2,BLUE),
-    Planet("Pluto",(0,1137.593-400),0.0146*(10**24),[VelConv*13356.0,0.0],2,TAN),
+    Planet("Sun",(0,0),1.989*(10**30),[0.0,0.0],1392000/(10**7),YELLOW),
+    Planet("Mercury",(0,6.98),0.330*(10**24),[VelConv*139896.0,0.0],4878/(10**7),TAN),
+    Planet("Venus",(0,10.89),4.87*(10**24),[VelConv*125244.0,0.0],12104/(10**7),WHITE),
+    Planet("Earth",(0,15.21),5.97*(10**24),[VelConv*105444.0,0.0],12756/(10**7),CADETBLUE),
+    Planet("Moon",(0,15.21+0.0406),0.073*(10**24),[VelConv*(3492+105444.0),0.0],1737/(10**7),GRAY),
+    Planet("Mars",(0,24.923),0.642*(10**24),[VelConv*79092.0,0.0],6787/(10**7),RUST),
+    Planet("Jupiter",(0,322.14-400),1898.0*(10**24),[VelConv*44784.0*(-1),0.0],142800/(10**7),PALEGOLD),
+    Planet("Saturn",(0,151.45),568.0*(10**24),[VelConv*32724.0,0.0],120000/(10**7),CORAL),
+    Planet("Uranus",(0,300.362),86.8*(10**24),[VelConv*23364.0,0.0],51200/(10**7),AQUA),
+    Planet("Neptune",(0,-454.567),102.0*(10**24),[VelConv*19332.0*(-1),0.0],48600/(10**7),BLUE),
+    Planet("Pluto",(0,737.593),0.0146*(10**24),[VelConv*13356.0,0.0],2300/(10**7),TAN),
     #Planet("Comet1",(500,300),102.0*(10**20),[VelConv*17548.0,0.0],2,GREEN),
     #Planet("Comet2",(500,500),102.0*(10**20),[VelConv*17548.0,0.0],2,GREEN),
     #Planet("Comet3",(500,1000),102.0*(10**20),[VelConv*7548.0,0.0],2,GREEN),
@@ -126,11 +131,11 @@ planets = [
     #Planet("Body5",(500,400),10.0*(10**29),[0.0,0.0],5,AQUA)
     ]
 def rendertext(body,cam):
-    BasicFont = pygame.font.SysFont(None,4*floor(cam.zoom))
+    BasicFont = pygame.font.SysFont(None,9)
     bodyname = BasicFont.render(body.name,True,WHITE,BLUE)
     bodynameRect = bodyname.get_rect()
     bodynameRect.centerx = floor(((body.loc[0])*cam.zoom)-cam.pos[0]+500)
-    bodynameRect.centery = floor(((body.loc[1])*cam.zoom)-cam.pos[1]+400)+5*cam.zoom
+    bodynameRect.centery = floor(((body.loc[1])*cam.zoom)-cam.pos[1]+405)
     windowSurface.blit(bodyname,bodynameRect)
 def renderall(planets,cam):
     for planet in planets:
@@ -138,8 +143,23 @@ def renderall(planets,cam):
 def scalepos(planets, zoom):
     for planet in planets:
         planet.loc[0] = zoom*planet.loc[0]
+def renderDisplay(cam,old,new):
+    BasicFont = pygame.font.SysFont(None,15)
+    scale = BasicFont.render(str(cam.zoom*scalefactor),True,WHITE,BLUE)
+    scaleRect = scale.get_rect()
+    scaleRect.centerx = 900
+    scaleRect.centery = 10
+    windowSurface.blit(scale,scaleRect)
+    frame = BasicFont.render(str(1/(new-old)),True,WHITE,BLUE)
+    frameRect = frame.get_rect()
+    frameRect.centerx = 900
+    frameRect.centery = 20
+    windowSurface.blit(frame,frameRect)
 def main():
+    old = time.time()
     camera = Camera([0,0],1)
+    currentplanet = 0
+    followplanets = False
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
@@ -158,19 +178,28 @@ def main():
                 if event.key == K_UP or event.key == K_w:
                     print("right")
                     camera.pos[1] -= 50
+                if event.key == K_c:
+                    if followplanets:
+                        currentplanet = (currentplanet+1)%len(planets)
+                if event.key == K_f:
+                    followplanets = not followplanets
+                        
             if event.type == MOUSEMOTION:
                 mousepos = pygame.mouse.get_pos
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 4:
                     print ("4")
-                    camera.zoom *= 1.125
-                    camera.pos[0] *= 1.125
-                    camera.pos[1] *= 1.125
+
+                    if camera.zoom < 1.125**70:
+                        camera.zoom *= 1.125
+                        camera.pos[0] *= 1.125
+                        camera.pos[1] *= 1.125
                 if event.button == 5:
                     print ("5")
-                    camera.zoom /= 1.125
-                    camera.pos[0] /= 1.125
-                    camera.pos[1] /= 1.125
+                    if camera.zoom > 1.125**(-15):
+                        camera.zoom /= 1.125
+                        camera.pos[0] /= 1.125
+                        camera.pos[1] /= 1.125
                     #dialation by factor 1/2 centered on cursor
                     
                     #global scalefactor
@@ -198,12 +227,17 @@ def main():
                     #G = 6.6741*(10**(-11)/(scalefactor**3))/(10**(-12))*6.91
         windowSurface.fill(BLACK)
         calculate_position(planets)
+        if followplanets:
+            camera.pos[0] = planets[currentplanet].loc[0]*camera.zoom
+            camera.pos[1] = planets[currentplanet].loc[1]*camera.zoom
         for target_planet in planets:
             target_planet.draw(windowSurface,camera,1)
             rendertext(target_planet,camera)
-
+        new = time.time()
+        renderDisplay(camera,old,new)
+        old = new
         pygame.display.update()
-        time.sleep(0.01)
+        #time.sleep(0.02)
 
 if __name__ == "__main__":
     main()
