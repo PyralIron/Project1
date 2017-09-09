126daad33c6a5b1b0d87c3c511cc3bbe8f587fa1
PyralIron <williamleber67@gmail.com>
2017-09-09 21:31:23 +0000

    added events + fixed velocity things, need to fix gravity

--- planets/main.cpp
+++ planets/main.cpp
@@ -9,6 +9,21 @@
 #include "planet.h"
 using namespace std;
 
+#define WINDOW_HEIGHT 500
+#define WINDOW_WIDTH 500
+
+bool handleevents() {
+	bool quit = false;
+	SDL_Event event;
+	while (SDL_PollEvent(&event)) {
+		switch (event.type) {
+		case SDL_QUIT:
+			quit = true;
+		}
+	}
+	return quit;
+}
+
 int main()
 {
 	if ( SDL_Init(SDL_INIT_EVERYTHING) != 0 )
@@ -16,7 +31,7 @@ int main()
                 cerr << SDL_GetError() << '\n';
                 return -1;
         }
-	SDL_Window *window = SDL_CreateWindow("", 0, 0, 100, 100, 0);
+	SDL_Window *window = SDL_CreateWindow("", 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, 0);
         if (window == NULL)
         {
                 cerr << SDL_GetError() << '\n';
@@ -31,27 +46,38 @@ int main()
 	SDL_Renderer *renderer = SDL_CreateSoftwareRenderer(surface);
 
 	vector<Planet> planetlist = {
-		Planet {"Earth", {0,0}, {0,0}, 100, 10, BLUE},
-		Planet {"Sun", {100,100}, {0,0}, 1000, 20, YELLOW},
-		Planet {"Mars", {200,50}, {5,0}, 100, 10, RED}
+		Planet {"Earth", {0,0}, {0.1,0.1}, 100, 10, BLUE}
+		//Planet {"Sun", {0,0}, {500,500}, 1000, 20, YELLOW},
+		//Planet {"Mars", {200,50}, {5,0}, 100, 10, RED}
 		};
 	vector<Vector> forcearray(planetlist.size(),{0,0});
-	while (true) {
+	bool quit = false;
+	while (!quit) {
+		quit = handleevents();
 		for (int i = 0; i < planetlist.size(); i++) {
 			//cout << x.getname();
 			forcearray[i] = {0,0};
-			for (auto y : planetlist) {
-				forcearray[i] += planetlist[i].gforce(y);
+			for (int j = 0; j < planetlist.size(); j++) {
+				if (i != j) {
+					forcearray[i] += planetlist[i].gforce(planetlist[j]);
+				}
 			}
 		}
 		for (int i = 0; i < planetlist.size(); i++) {
 			auto F = forcearray[i];
-			planetlist[i].setvelocity() = planetlist[i].getvelocity() + F/planetlist[i].getmass();
-			planetlist[i].setposition() = planetlist[i].getposition() + planetlist[i].getvelocity();
+			planetlist[i].velocity() = planetlist[i].getvelocity() + F/planetlist[i].getmass();
+			planetlist[i].position() = planetlist[i].getposition() + planetlist[i].getvelocity();
 		}
 		SDL_FillRect(surface, NULL, BLACK);
 		for (int i = 0; i < planetlist.size(); i++) {
-			SDL_Rect rect = {planetlist[i].getposition()[0]-planetlist[i].getradius(),planetlist[i].getposition()[1]-planetlist[i].getradius(),planetlist[i].getradius()*2,planetlist[i].getradius()*2};
+			auto p = planetlist[i];
+			cout << p.getname() << ' ';
+			cout << p.getposition()[0] << ' ';
+			cout << p.getposition()[1] << '\n';
+			cout << p.getvelocity()[0] << ' ' << p.getvelocity()[1] << '\n';
+			Point rp = planetlist[i].getposition();
+			int rr = planetlist[i].getradius();
+			SDL_Rect rect = {rp[0]-rr,rp[1]-rr,rr*2,rr*2};
 			SDL_FillRect(surface, &rect, planetlist[i].getcolor());
 		}
 		SDL_UpdateWindowSurface(window);
--- planets/planet.h
+++ planets/planet.h
@@ -12,24 +12,24 @@ class Planet {
 public:
 	std::string name;
 	Point pos;
-	Vector velocity;
+	Vector vel;
 	long double mass;
 	long double radius;
 	Color color;
 public:
 	//Planet (std::string _name="", Point _pos={0,0}, Vector _velocity={0,0}, long double _radius=0, long double _mass = 0, Color _color=YELLOW), name(_name), pos(_pos), velocity(_velocity), radius(_radius), mass(_mass), color(_color) {};
-	static constexpr long double G {6.67408*0.00000000001};
+	static constexpr long double G {6.67408};
 	Vector gforce(Planet b);
 
 	const Point &getposition() {return this->pos;}
-	const Vector getvelocity() {return this->velocity;}
+	const Vector getvelocity() {return this->vel;}
 	long double getmass() {return this->mass;}
 	std::string getname() {return this->name;}
 	Color getcolor() {return this->color;}
 	const long double &getradius() {return this->radius;}
 
-	Point &setposition() {return this->pos;}
-	Vector &setvelocity() {return this->velocity;}
+	Point &position() {return this->pos;}
+	Vector &velocity() {return this->vel;}
 };
 
 #endif
